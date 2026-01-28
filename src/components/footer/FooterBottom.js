66008f1cabaa1723f03d966269f758773882dba1
Youra No AI <154102639+YouraNoAI@users.noreply.github.com>
2026-01-28 13:50:07 +0700

    portofolio

--- src/components/footer/FooterBottom.js
+++ src/components/footer/FooterBottom.js
@@ -3,7 +3,7 @@ import React from "react";
 const FooterBottom = () => {
   return (
     <div className="w-full py-10">
-      <p className="text-center text-gray-500 text-base">© 2022. All rights reserved by Youra No AI</p>
+      <p className="text-center text-gray-500 text-base">© 2026. All rights reserved by Youra No AI</p>
     </div>
   );
 };
